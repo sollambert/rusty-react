a3800f53af3004afba97c2c2b40b42294dce6822
Solomon Lambert <sol.lambert@gmail.com>
2023-05-19 15:30:16 -0500

    added comments to find_divisors module to be more descriptive of what it's doing

--- src/modules/find_divisors.rs
+++ src/modules/find_divisors.rs
@@ -12,6 +12,12 @@ extern "C" {
 
 //Bindgen export for find_divisors function
 #[wasm_bindgen]
+/**
+* @param {u64} min
+* @param {u64} max
+* @param {bool} display_time
+* function that calls list_squared and displays time to execute if display_time is true
+*/
 pub fn find_divisors(min: u64, max: u64, display_time: bool) {
     let start = js_sys::Date::now();
     log(&format!("{:?}", list_squared(min, max)));
@@ -20,9 +26,17 @@ pub fn find_divisors(min: u64, max: u64, display_time: bool) {
     }
 }
 
-fn list_squared(m: u64, n: u64) -> Vec<(u64, u64)> {
+/**
+ * @param {u64} min
+ * @param {u64} max
+ * @return {Vec<(u64, u64)>}
+ * Iterates through all numbers between min and max inclusively
+ * looking for any numbers who's squared divisors sum to a perfect square
+ * then returns all matching numbers as a tuple
+ */
+fn list_squared(min: u64, max: u64) -> Vec<(u64, u64)> {
     let mut tuples: Vec<(u64, u64)> = vec![];
-    for num in m..n+1 {
+    for num in min..max+1 {
         let mut divisors = get_divisors(num);
         square_divisors(&mut divisors);
         let sum = divisors.iter().sum();
@@ -38,13 +52,23 @@ fn list_squared(m: u64, n: u64) -> Vec<(u64, u64)> {
     tuples
 }
 
+/**
+ * @param {&Vec<u64>} divisors
+ * @return {&Vec<u64>}
+ * Squares all elements of given vector and returns as reference
+ */
 fn square_divisors<'a>(divisors: &'a mut Vec<u64>) -> &'a Vec<u64> {
     for index in 0..divisors.len() {
         divisors[index] = divisors[index] * divisors[index];
     }
-        divisors
-    }
+    divisors
+}
 
+/**
+ * @param {u64} num
+ * @return {Vec<u64>}
+ * Returns a vector of all divisors of supplied u64 number
+ */
 fn get_divisors(num: u64) -> Vec<u64> {
     let mut divisor = 1;
     let mut divisors: Vec<u64> = vec![];
