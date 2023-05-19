72fec56db6cc442fbfc94011b9e36db85af8e630
Solomon Lambert <sol.lambert@gmail.com>
2023-05-19 14:40:34 -0500

    change index.tsx to use new createRoot syntax for React v18

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import ReactDOM from "react-dom";
 import { App } from "./App";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
